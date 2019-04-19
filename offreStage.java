9fb60676228e04717462da042d8599dad7ef0d90
LePacko <canovasromain@gmail.com>
2019-04-19 10:52:29 +0200

    Entreprise.java

---
+++ offreStage.java
@@ -0,0 +1,11 @@
+package projetTp;
+
+public class offreStage {
+	private int id;
+	private String descriptif;
+	private String titre;
+	private int date_debut;
+	private int duree;
+	
+	public offreStage()
+}
